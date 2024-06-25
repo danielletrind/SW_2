be3142399a8184281d957d566e3c5a85408e7807
Danielle Trindade <100931893+danielletrind@users.noreply.github.com>
2024-06-25 11:03:32 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    [
+        'nome' => 'Danielle',
+        'email' => 'danielle@gmail' 
+    ],
+    [
+        'nome' => 'Andreia',
+        'email' => 'andreia@gmail' 
+    ],
+    [
+        'nome' => 'Daniel',
+        'email' => 'daniel@gmail' 
+    ],
+    [
+        'nome' => 'Ana',
+        'email' => 'ana@gmail' 
+    ],
+    [
+        'nome' => 'Ariany',
+        'email' => 'ariany@gmail' 
+    ],
+    [
+        'nome' => 'Isadora',
+        'email' => 'isa@gmail' 
+    ],
+    [
+        'nome' => 'Ana Laura',
+        'email' => 'ana.laura@gmail' 
+    ],
+    [
+        'nome' => 'Maria',
+        'email' => 'mary@gmail' 
+    ],
+    [
+        'nome' => 'Maria Rita',
+        'email' => 'maria.rita@gmail' 
+    ],
+    [
+        'nome' => 'Thifany',
+        'email' => 'thifao@gmail' 
+    ],
+
+];
+
+?>
